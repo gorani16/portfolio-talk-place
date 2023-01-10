33fc9e9331af498c2e52ac243fcb06f294c9fe44
gorani16 <jgee0116@naver.com>
2023-01-11 00:06:23 +0900

    - Splash Activity 구현

---
+++ app/src/main/java/com/gorani/talkplace/auth/IntroActivity.kt
@@ -0,0 +1,13 @@
+package com.gorani.talkplace.auth
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gorani.talkplace.R
+
+class IntroActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intro)
+    }
+}
