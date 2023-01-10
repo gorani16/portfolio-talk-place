f7febb21a509faaec93135eb437bf37b06be0fda
gorani16 <jgee0116@naver.com>
2023-01-11 03:25:13 +0900

    - 로그인, 회원가입 액티비티 레이아웃 구현

--- app/src/main/java/com/gorani/talkplace/auth/IntroActivity.kt
+++ app/src/main/java/com/gorani/talkplace/auth/IntroActivity.kt
@@ -1,13 +1,31 @@
 package com.gorani.talkplace.auth
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.gorani.talkplace.R
+import com.gorani.talkplace.databinding.ActivityIntroBinding
 
 class IntroActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityIntroBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_intro)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_intro)
+
+        binding.btnLogin.setOnClickListener {
+            val intent = Intent(this, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+        binding.btnJoin.setOnClickListener {
+            val intent = Intent(this, JoinActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
---
+++ app/src/main/java/com/gorani/talkplace/auth/JoinActivity.kt
@@ -0,0 +1,13 @@
+package com.gorani.talkplace.auth
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gorani.talkplace.R
+
+class JoinActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_join)
+    }
+}
