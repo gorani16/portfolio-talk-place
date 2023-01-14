322a37169d61a8a9170f03209da8efb7d3c7a4cb
gorani16 <jgee0116@naver.com>
2023-01-15 04:34:59 +0900

    - 로그아웃 기능 구현

--- app/src/main/java/com/gorani/talkplace/MainActivity.kt
+++ app/src/main/java/com/gorani/talkplace/MainActivity.kt
@@ -3,6 +3,7 @@ package com.gorani.talkplace
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.ui.setupWithNavController
@@ -10,6 +11,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
+import com.gorani.talkplace.setting.SettingActivity
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,8 +21,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        auth = Firebase.auth
-
         val bottomNavigationView = findViewById<BottomNavigationView>(R.id.navigation_main)
 
         val navController = supportFragmentManager.findFragmentById(R.id.container_main)?.findNavController()
@@ -28,14 +28,11 @@ class MainActivity : AppCompatActivity() {
             bottomNavigationView.setupWithNavController(it)
         }
 
-//        binding.btnLogout.setOnClickListener {
-//            auth.signOut()
-//
-//            val intent = Intent(this, IntroActivity::class.java)
-//            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-//            startActivity(intent)
-//        }
+        val logoutBtn = findViewById<ImageView>(R.id.iv_toolbar_menu)
+        logoutBtn.setOnClickListener {
+            val intent = Intent(this, SettingActivity::class.java)
+            startActivity(intent)
+        }
 
     }
-
 }
---
+++ app/src/main/java/com/gorani/talkplace/setting/SettingActivity.kt
@@ -0,0 +1,36 @@
+package com.gorani.talkplace.setting
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.gorani.talkplace.R
+import com.gorani.talkplace.auth.IntroActivity
+
+class SettingActivity: AppCompatActivity() {
+
+    private lateinit var auth: FirebaseAuth
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_setting)
+
+        auth = Firebase.auth
+
+        val logoutBtn = findViewById<Button>(R.id.btn_logout)
+        logoutBtn.setOnClickListener {
+
+            auth.signOut()
+            Toast.makeText(this, "로그아웃", Toast.LENGTH_LONG).show()
+
+            val intent = Intent(this, IntroActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
+    }
+}
